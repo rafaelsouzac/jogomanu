0629d097907ea2cbbed0ddcb1053eceb7cf52343
Rafael Correa <rafaelsouzac@gmail.com>
2026-06-25 09:05:03 -0300

    Commit comp E 260626 0904

--- docs/query_json_cadastro_perguntas.sql
+++ docs/query_json_cadastro_perguntas.sql
@@ -1,46 +1,45 @@
-#USANDO JSON PARA CADASTRO DE PERGUNTAS E RESPOSTAS
-#UTILIZANDO AS FUNÇÕES NATIVAS DO MARIADB PARA ESTE PROPOSITO
+-- procedimento para inserir disciplina >> usp_cadastro_disciplinas(parametros) OOOKKKK
+-- procedimento para inserir conteúdo vinculado a disciplina >> usp_cadastro_conteudos(@id_disciplina, parametros) OOOKKKK
+-- procedimento para inserir pergunta vinculada a disciplina >> usp_cadastro_perguntas(@id_disciplina, @id_origem, parametros)perguntas
+-- procedimento para inserir respostas vinculada a pergunta >> usp_cadastro_respostas(@id_pergunta, json)
 
-SET @cad_disciplinas = json_object(
-	'pergunta', json_object(
-		'enunciado', '', 
-        'localorigin', '',
-        'temimagem', '', 
-        'fk_disciplina', ''
-    ),
+
+
+
+SET @cad_respostas = json_object(
     'resposta', json_array(
 		-- Cadas Objeto representa uma alternativa de resposta.
         -- A
 		json_object(
-			'fk_pergunta',0,
+			'fk_pergunta', @id_pergunta,
 			'status', '', -- I para incorreta e C para correta.
 			'temimagem', '', -- 0 Não contem imagem 1 contem.
 			'enunciado_resposta', '' -- Limitado a 1020 caracteres
 		),
         -- B
 		json_object(
-			'fk_pergunta',0,
+			'fk_pergunta', @id_pergunta,
 			'status', '', 
 			'temimagem', '', 
 			'enunciado_resposta', '' 
 		),
         -- C
 		json_object(
-			'fk_pergunta',0,
+			'fk_pergunta', @id_pergunta,
 			'status', '',
 			'temimagem', '', 
 			'enunciado_resposta', '' 
 		),
         -- D
 		json_object(
-			'fk_pergunta',0,
+			'fk_pergunta', @id_pergunta,
 			'status', '', 
 			'temimagem', '', -- 0 Não contem imagem 1 contem.
 			'enunciado_resposta', '' -- Limitado a 1020 caracteres
 		),
         -- E
 		json_object(
-			'fk_pergunta',0,
+			'fk_pergunta', @id_pergunta,
 			'status', '', -- I para incorreta e C para correta.
 			'temimagem', '', -- 0 Não contem imagem 1 contem.
 			'enunciado_resposta', '' -- Limitado a 1020 caracteres
