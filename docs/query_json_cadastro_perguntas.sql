5b46dd43579b4c5329666e64c8ebc15f35d211e0
Rafael Corrêa <rafaelsouzac@gmail.com>
2026-06-25 23:14:00 -0300

    Commit comp C 250626 2313

--- docs/query_json_cadastro_perguntas.sql
+++ docs/query_json_cadastro_perguntas.sql
@@ -1,18 +1,14 @@
--- procedimento para inserir disciplina >> usp_cadastro_disciplinas(parametros) OOOKKKK
--- procedimento para inserir conteúdo vinculado a disciplina >> usp_cadastro_conteudos(@id_disciplina, parametros) OOOKKKK
--- procedimento para inserir pergunta vinculada a disciplina >> usp_cadastro_perguntas(@id_disciplina, @id_origem, parametros)perguntas
--- procedimento para inserir respostas vinculada a pergunta >> usp_cadastro_respostas(@id_pergunta, json)
 
-
-
-
-SET @cad_respostas = json_object(
-    'resposta', json_array(
+SET @cad_pergunta_respostas = json_object(
+    'enunciado', '',
+    'temimagem', 0,
+	'fk_disciplina', 01,
+    'respostas', json_array(
 		-- Cadas Objeto representa uma alternativa de resposta.
         -- A
 		json_object(
 			'fk_pergunta', @id_pergunta,
-			'status', '', -- I para incorreta e C para correta.
+			'status_resposta', '', -- I para incorreta e C para correta.
 			'temimagem', '', -- 0 Não contem imagem 1 contem.
 			'enunciado_resposta', '' -- Limitado a 1020 caracteres
 		),
