9f50f34d2598b02e74ec14aa68f545cda4071a6b
Rafael Correa <rafaelsouzac@gmail.com>
2026-08-08 10:00:15 -0300

    commit comp E 0808 1000

--- docs/query_inserir_pergunta_resposta.sql
+++ docs/query_inserir_pergunta_resposta.sql
@@ -1,25 +1,19 @@
 -- PK_CONTEUDO   DESCRICAO
 --     7           Elementos da comunicação
 
--- SELECT * FROM CONTEUDOS;
--- select * from perguntas;
-
-SELECT * FROM RESPOSTAS;
-
-
 SET @enunciado = 'Qual é o traço visual mais característico do personagem Menino Maluquinho?';        -- Enunciado da pergunda
 SET @temimagem = '0';  -- Sempre é char(1) igual a zero
-SET @fk_conteudo = 7;   -- Já será informado
-SET @fk_gerado = 0;       -- parâmetro de retorno da sp
+SET @fk_conteudo = 7;  -- Já será informado
+SET @fk_gerado = 0;    -- parâmetro de retorno da sp
 
  CALL usp_inserir_pergunta(@enunciado, @temimagem, @fk_conteudo, @fk_pergunta);
 
  SET @fk_pergunta = 81;
- SET @alternativa_a = 'A panela na cabeça.'; -- varchar(1020) enunciado
- SET @alternativa_b = 'Um boné azul.'; -- varchar(1020) enunciado
- SET @alternativa_c = 'Um chapéu de palha.'; -- varchar(1020) enunciado
+ SET @alternativa_a = 'A panela na cabeça.';       -- varchar(1020) enunciado
+ SET @alternativa_b = 'Um boné azul.';             -- varchar(1020) enunciado
+ SET @alternativa_c = 'Um chapéu de palha.';       -- varchar(1020) enunciado
  SET @alternativa_d = 'Um capacete de bicicleta.'; -- varchar(1020) enunciado
- SET @alternativa_e = 'Uma boina.'; -- varchar(1020) enunciado
+ SET @alternativa_e = 'Uma boina.';                -- varchar(1020) enunciado
 
  SET @status_a = 'C';  -- char(1) utiliza-se I para aletrenatuva incorreta
  SET @status_b = 'I';  
